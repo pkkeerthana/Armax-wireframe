7dea7d8acfc47065b07f745147d48a1effd30466
keerthana <keerthananambiar1234@gmail.com>
2020-08-14 13:04:13 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LinegraphComponent } from './linegraph/linegraph.component';
+import { HeatmapComponent } from './heatmap/heatmap.component';
+import { EventstableComponent } from './eventstable/eventstable.component';
+import { ControltableComponent } from './controltable/controltable.component';
+import { HeaderComponent } from './header/header.component';
+import { SubheaderComponent } from './subheader/subheader.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinegraphComponent,
+    HeatmapComponent,
+    EventstableComponent,
+    ControltableComponent,
+    HeaderComponent,
+    SubheaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    HighchartsChartModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSlideToggleModule,
+    FormsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
